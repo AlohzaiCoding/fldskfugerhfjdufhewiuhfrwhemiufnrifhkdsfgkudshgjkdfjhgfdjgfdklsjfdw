a56affdd71b041c9d849cd003fee622b87577a00
AlohzaiCoding <tooncubemailbox@gmail.com>
2026-08-16 13:33:34 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-const SB_URL='https://gbdfaibmjtpxnbbhacgz.supabase.co';
-const SB_KEY=(function(){
-  const _0=['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9','eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdiZGZhaWJtanRweG5iYmhhY2d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4MzE5MzIsImV4cCI6MjEwMjQwNzkzMn0','7uUMf3ZVWUtWlo3RcAzolWWH3DiafqRr5Pn_iPes-Io'];
-  return _0.join('.');
-})();
-const HDRS={'apikey':SB_KEY,'Authorization':'Bearer '+SB_KEY,'Content-Type':'application/json','Prefer':'return=representation'};
-const BLOCKED_NAMES=['bento','cyano','blueno','bentothecoder','bluenothecoder','bento the coder','blueno the coder'];
